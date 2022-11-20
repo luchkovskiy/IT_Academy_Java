cd89277c1c000a44829135e14261ed16500efb0f
Alexey Luchkovskiy <116020666+luchkovskiy@users.noreply.github.com>
2022-11-20 19:21:03 +0300

    Delete src/com/java/Lessons/lesson1 directory

--- src/com/java/Lessons/lesson1/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-package com.java.Lessons.lesson1;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        int salary = 5000;
-        System.out.println("Salary is " + salary);
-    }
-}
